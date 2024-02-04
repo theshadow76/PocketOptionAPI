b0ecbc56c867c1215c33242408d5972db1e9585c
Vigo Walker <vigopaul05@gmail.com>
2024-02-04 13:10:42 -0400

    sffsesfdfe

---
+++ pocketoptionapi/constants.py
@@ -0,0 +1,32 @@
+import random
+
+class REGION:
+    
+    REGIONS = {
+        "SEYCHELLES": "wss://api-sc.po.market/socket.io/?EIO=4&transport=websocket",
+        "HONGKONG": "wss://api-hk.po.market/socket.io/?EIO=4&transport=websocket",
+        "SERVER1": "wss://api-spb.po.market/socket.io/?EIO=4&transport=websocket",
+        "FRANCE2": "wss://api-fr2.po.market/socket.io/?EIO=4&transport=websocket",
+        "UNITED_STATES2": "wss://api-us4.po.market/socket.io/?EIO=4&transport=websocket",
+        "UNITED_STATES3": "wss://api-us3.po.market/socket.io/?EIO=4&transport=websocket",
+        "UNITED_STATES2": "wss://api-us2.po.market/socket.io/?EIO=4&transport=websocket",
+        "RUSSIA": "wss://api-msk.po.market/socket.io/?EIO=4&transport=websocket",
+        "SERVER2": "wss://api-l.po.market/socket.io/?EIO=4&transport=websocket",
+        "INDIA": "wss://api-in.po.market/socket.io/?EIO=4&transport=websocket",
+        "FRANCE": "wss://api-fr.po.market/socket.io/?EIO=4&transport=websocket",
+        "FINLAND": "wss://api-fin.po.market/socket.io/?EIO=4&transport=websocket",
+        "SERVER3": "wss://api-c.po.market/socket.io/?EIO=4&transport=websocket",
+        "ASIA": "wss://api-asia.po.market/socket.io/?EIO=4&transport=websocket",
+    }
+
+
+    def __getattr__(self, key):
+        try:
+            return self.REGIONS[key]
+        except KeyError:
+            raise AttributeError(f"'{self.REGIONS}' object has no attribute '{key}'")
+    
+    def get_regions(self, randomize: bool = True):
+        if randomize:
+            return sorted(list(self.REGIONS.values()), key=lambda k: random.random())
+        return list(self.REGIONS.values())
