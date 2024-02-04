e7ed949af0ba779b442720cf66e8df58ae9e4a72
Vigo Walker <vigopaul05@gmail.com>
2024-02-04 12:32:34 -0400

    awfwef

---
+++ pocketoptionapi/backend/ws/client.py
@@ -0,0 +1,26 @@
+import websocket
+
+class WebSocketClient:
+    def __init__(self, url):
+        self.url = url
+        websocket.enableTrace(True)
+        self.ws = websocket.WebSocketApp(self.url,
+                                         on_open=self.on_open,
+                                         on_message=self.on_message,
+                                         on_error=self.on_error,
+                                         on_close=self.on_close)
+
+    def on_message(self, ws, message):
+        print(message)
+
+    def on_error(self, ws, error):
+        print(error)
+
+    def on_close(self, ws, close_status_code, close_msg):
+        print("### closed ###")
+
+    def on_open(self, ws):
+        print("Opened connection")
+
+    def run(self):
+        self.ws.run_forever()  # Use dispatcher for automatic reconnection
--- pocketoptionapi/pocket.py
+++ pocketoptionapi/pocket.py
@@ -1,2 +1,11 @@
 # Made by © Vigo Walker
+from pocketoptionapi.backend.ws.client import WebSocketClient
 
+class PocketOptionApi:
+    def __init__(self, token) -> None:
+        self.ws_url = "wss://api-fin.po.market/socket.io/?EIO=4&transport=websocket"
+        self.token = token
+        self.client = WebSocketClient(self.ws_url)
+    def connect(self):
+        # Init the websocket
+        self.client.run()
