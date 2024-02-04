a24fe052f476176cadc6dfd7681d1f1675249890
Vigo Walker <vigopaul05@gmail.com>
2024-02-04 19:12:07 -0400

    gvv

--- pocketoptionapi/backend/ws/client.py
+++ pocketoptionapi/backend/ws/client.py
@@ -1,19 +1,31 @@
 import websocket
 from pocketoptionapi.constants import REGION
 import threading
+import logging
 import ssl
 
 class WebSocketClient:
-    def __init__(self, url):
+    def __init__(self, url, pocket_api_instance=None):
         self.url = url
+        self.pocket_api_instance = pocket_api_instance
+        self.logger = logging.getLogger(__name__)
+        self.logger.setLevel(logging.INFO)
+        formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
+
+        # Create file handler and add it to the logger
+        file_handler = logging.FileHandler('pocket.log')
+        file_handler.setFormatter(formatter)
+        self.logger.addHandler(file_handler)
         self.ws = websocket.WebSocketApp(self.url,
                                          on_open=self.on_open,
                                          on_message=self.on_message,
                                          on_error=self.on_error,
                                          on_close=self.on_close)
+        self.logger.info("Starting websocket client...")
     def reconnect(self):
         # List of regions to try
         print("Reconecting...")
+        self.logger.info("Reconnecting...")
         REG = REGION()
         regions = REG.get_regions()
 
@@ -50,17 +62,23 @@ class WebSocketClient:
 
     def on_message(self, ws, message):
         print(message)
+        self.logger.info(f"Recieved a message!: {message}")
 
     def on_error(self, ws, error):
         print(error)
+        self.logger.error(f"Got a error: {error}")
         self.reconnect()
 
     def on_close(self, ws, close_status_code, close_msg):
         print("### closed ###")
+        self.logger.warning(f"Connection closed, conections status_code: {close_status_code} and the message is: {close_msg}")
         self.reconnect()
 
     def on_open(self, ws):
         print("Opened connection")
+        self.logger.info("Opened!")
+        if self.pocket_api_instance:
+            self.pocket_api_instance.connected_event.set()
 
     def run(self):
         self.ws.run_forever()  # Use dispatcher for automatic reconnection
--- pocketoptionapi/pocket.py
+++ pocketoptionapi/pocket.py
@@ -13,6 +13,7 @@ class PocketOptionApi:
     def __init__(self, token) -> None:
         self.ws_url = "wss://api-fin.po.market/socket.io/?EIO=4&transport=websocket"
         self.token = token
+        self.connected_event = threading.Event()
         self.client = WebSocketClient(self.ws_url)
         self.logger = logging.getLogger(__name__)
         self.logger.setLevel(logging.INFO)
@@ -24,9 +25,49 @@ class PocketOptionApi:
         self.logger.addHandler(file_handler)
 
         self.logger.info(f"initialiting Pocket API with token: {token}")
+
+        self.ping_thread = threading.Thread(target=self.auto_ping)  # Create a new thread for auto_ping
+        self.ping_thread.daemon = True  # Set the thread as a daemon so it will terminate when the main program terminates
+        self.ping_thread.start()  # Start the auto_ping thread
+    def auto_ping(self):
+        self.logger.info("Starting auto ping thread")
+
+        while True:
+            pause.seconds(5)  # Assuming that you've imported 'pause'
+            try:
+                if self.websocket_client.ws.sock and self.websocket_client.ws.sock.connected:  # Check if socket is connected
+                    self.ping()
+                else:
+                    self.logger.warning("WebSocket is not connected. Attempting to reconnect.")
+                    # Attempt reconnection
+                    if self.connect():
+                        self.logger.info("Successfully reconnected.")
+                    else:
+                        self.logger.warning("Reconnection attempt failed.")
+                    try:
+                        self.ping()
+                        self.logger.info("Sent ping reuqests successfully!")
+                    except Exception as e:
+                        self.logger.error(f"A error ocured trying to send ping: {e}")
+            except Exception as e:  # Catch exceptions and log them
+                self.logger.error(f"An error occurred while sending ping or attempting to reconnect: {e}")
+                try:
+                    self.logger.warning("Trying again...")
+                    v1 = self.connect()
+                    if v1:
+                        self.logger.info("Conection completed!, sending ping...")
+                        self.ping()
+                    else:
+                        self.logger.error("Connection was not established")
+                except Exception as e:
+                    self.logger.error(f"A error ocured when trying again: {e}")
+
+            pause.seconds(5)  # Assuming that you've imported 'pause'
+
     def connect(self, msg):
+        self.logger.info("Attempting to connect...")
         try:
-            self.websocket_client = WebSocketClient(self.ws_url)
+            self.websocket_client = WebSocketClient(self.ws_url, pocket_api_instance=self)
 
             self.websocket_thread = threading.Thread(target=self.websocket_client.ws.run_forever, kwargs={
                 'sslopt': {
@@ -43,15 +84,23 @@ class PocketOptionApi:
             self.websocket_thread.daemon = True
             self.websocket_thread.start()
 
+            self.websocket_client.ws.keep_running = True
+            self.websocket_client.ws.run_forever
+
             self.send_websocket_request(msg=msg)
+            self.websocket_client.reconnect()
+            self.logger.info("Connection successful.")
         except Exception as e:
             print(f"Going for exception.... error: {e}")
+            self.logger.error(f"Connection failed with exception: {e}")
+
             self.websocket_client.reconnect()
             self.send_websocket_request(msg=msg)
     def send_websocket_request(self, msg):
         """Send websocket request to PocketOption server.
         :param dict msg: The websocket request msg.
         """
+        self.logger.info(f"Sending websocket request: {msg}")
         def default(obj):
             if isinstance(obj, decimal.Decimal):
                 return str(obj)
@@ -59,6 +108,24 @@ class PocketOptionApi:
 
         data = json.dumps(msg, default=default)
 
-        self.websocket_client.ws.send(bytearray(urllib.parse.quote(data).encode('utf-8')), opcode=websocket.ABNF.OPCODE_BINARY)
-        pause.seconds(5)
+        try:
+            self.logger.info("Request sent successfully.")
+            self.websocket_client.ws.send(bytearray(urllib.parse.quote(data).encode('utf-8')), opcode=websocket.ABNF.OPCODE_BINARY)
+            pause.seconds(5)
+            return True
+        except Exception as e:
+            self.logger.error(f"Failed to send request with exception: {e}")
+            # Consider adding any necessary exception handling code here
+            try:
+                self.websocket_client.reconnect()
+                self.websocket_client.ws.send(bytearray(urllib.parse.quote(data).encode('utf-8')), opcode=websocket.ABNF.OPCODE_BINARY)
+                pause.seconds(5)
+            except Exception as e:
+                self.logger.warning(f"Was not able to reconnect: {e}")
+                pause.seconds(5)
+
+    @property
+    def ping(self):
+        self.send_websocket_request(msg="3")
+        self.logger.info("Sent a ping request")
         return True
