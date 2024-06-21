3a4010c7aaa2c6d6ae4b5d03fc85efb352087f10
vigo walker <vigopaul05@gmail.com>
2024-06-21 14:36:59 -0400

    fssf

--- test.py
+++ test.py
@@ -2,30 +2,16 @@ import random
 import time
 
 from pocketoptionapi.stable_api import PocketOption
-
-ssid = (r'')
+import logging
+logging.basicConfig(level=logging.DEBUG,format='%(asctime)s %(message)s')
+ssid = (r'42["auth",{"session":"vtftn12e6f5f5008moitsd6skl","isDemo":1,"uid":27658142,"platform":1}]')
 api = PocketOption(ssid)
-
-
-def direction():
-    # Selecciona aleatoriamente entre 'call' y 'put'
-    return random.choice(['call', 'put'])
 
 
 if __name__ == "__main__":
     api.connect()
-    time.sleep(2)
+    time.sleep(5)
 
     print(api.check_connect(), "check connect")
 
-    data_candles = api.get_candles("AUDNZD_otc", 60, time.time(), count_request=1)
-
-    data, diff = api.process_candle(data_candles, 60)
-    print(data)
-    print(diff)
-    data.to_csv('datos_AUDNZD_otc_test.csv', index=False)
-    while api.check_connect():
-        print(api.get_server_timestamp(), "server datetime")
-        time.sleep(1)
-
-    # Cierra la conexión con la API
+    print(api.get_balance())
